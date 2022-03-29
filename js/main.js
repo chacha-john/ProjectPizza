c83a854d92f520c62a67f433484021aa75a47b0b
rikonnect <chachaerickjo@gmail.com>
2022-03-29 12:32:41 +0300

    pt - fix the bug which doubled the payable amount for home deliveries

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 let orderData = document.querySelector("form#orderForm")
-let deliveryDetails = document.querySelector("form#location")
 let orders = []
 let overallcost
 function Order(pizzaType, pizzaCrust,pizzaSize,pizzaToppings){
@@ -9,6 +8,7 @@ function Order(pizzaType, pizzaCrust,pizzaSize,pizzaToppings){
     this.pizzaToppings = pizzaToppings
     this.total = parseInt(pizzaCrust) + parseInt(pizzaType) + parseInt(pizzaSize) + parseInt(pizzaToppings)
 }
+
 orderData.addEventListener("submit",(e)=>{
     e.preventDefault()
     let pizzaType = orderData.typeOfPizza.value
@@ -23,38 +23,40 @@ orderData.addEventListener("submit",(e)=>{
     findTotal = (total,num)=>{
         return total + num
     }
-    overallcost = orders.reduce(findTotal,0);
+    overallcost = orders.reduce(findTotal);
     
     document.querySelector("#submit").style.display = "none"
     document.querySelector(".cost").textContent = "Your order goes at Sh. " + overallcost
     document.querySelector("#control").style.display = "flex"
 
-    document.querySelector("#checkout").addEventListener("click",(e)=>{
-        e.preventDefault()
-        prompt = "Thank you for shopping with us! Your total cost is Sh. " + overallcost + ". Please visit our store within 12 hours to collect your order!"
-        document.querySelector("#order").style.display = "none"
-        document.querySelector("#output").textContent = prompt
-    
-    })
 })
 
-deliveryDetails.addEventListener("submit",(e)=>{
+document.querySelector("#checkout").addEventListener("click",(e)=>{
     e.preventDefault()
+    prompt = "Thank you for shopping with us! Your total cost is Sh. " + overallcost + ". Please visit our store within 12 hours to collect your order!"
+    document.querySelector("#order").style.display = "none"
+    document.querySelector("#output").textContent = prompt
 
-    let username = deliveryDetails.username.value
-    let residence = deliveryDetails.residence.value
+})
+let deliveryDetails = document.querySelector("form#location")
 
-    prompt = "Thank you for shopping with us, " + username +"! Your total cost is Sh. " + overallcost + ". Your delivery will be made at " + residence + " within 12 hours!"
+document.querySelector("form#location").addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let jina = deliveryDetails.username.value
+    let makaazi = deliveryDetails.residence.value
+
+    let totalPayable = overallcost + 250 //additional fee for shipping
+
+    prompt = "Thank you , " + jina + ", for shopping with us. Your total cost is " + totalPayable + " which will be paid on delivery at your residence in " + makaazi + "! Thank you!"
+
+    document.querySelector("form#location").style.display = "none" //hide form used to collect user data
     alert(prompt)
+
 })
 
 document.querySelector("#delivery").addEventListener("click",(e)=>{
+    e.preventDefault()
     document.querySelector("form#location").style.display = "block"
     document.querySelector("#order").style.display = "none"
-
-})
-document.querySelector("#checkout").addEventListener("click",(e)=>{
-    e.preventDefault()
-    document.querySelector("#output").textContent = "Your total cost is Sh. " + order.total
 
 })
